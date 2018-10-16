f418c1463f55247980404179ef4e0049b0b9abf6
Jack Rookes <jr1175@york.ac.uk>
2018-10-16 15:02:38 +0100

    Add lab 5 stuff

--- lab2/data-storage-62M/lab5/mempool.c
+++ lab2/data-storage-62M/lab5/mempool.c
@@ -2,13 +2,22 @@
 
 void pool_init(pool_t *pool) {
     // Initialise the pool
+    pool->head = NULL;
 }
 
 void *pool_allocate(pool_t *pool) {
     // Return the head of the list of blocks
     // Update the head pointer
+    void* oldHead = pool->head;
+    pool->head = *((void**)oldHead);
+    return oldHead;
+    
 }
 
 void pool_deallocate(pool_t *pool, void *item) {
     // Add the new item to the head of the list
+    void* oldHead = pool->head;
+    pool->head = item;
+    *((void**)item) = oldHead;
+    
 }
